5474af1412235d6014400b97514a3fa0c94356bf
peterdeme <snypox@gmail.com>
2022-07-29 09:54:11 +0200

    Add test case

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 resource "random_password" "pwd" {
+    length = 5
 }
 
 resource "random_password" "pwd2" {
+    length = 5
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "petname" {
     value = random_password.pwd
+    sensitive = true
 }
